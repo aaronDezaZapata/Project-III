debaa00f52435bb32bae3f63542373c8d0fc5b21
Aarón Deza Zapata <aaron.deza@estudiant.enti.cat>
2026-01-14 03:10:17 +0100

    Decal material system done and working with player

--- Assets/Scripts/DecalManager.cs
+++ Assets/Scripts/DecalManager.cs
@@ -1,27 +1,31 @@
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class DecalManager : MonoBehaviour
 {
-    private void OnEnable()
+    [SerializeField] private Material blackDecalMat;
+    [SerializeField] private Material blueDecalMat;
+    [SerializeField] private Material greenDecalMat;
+    [SerializeField] private Material greyDecalMat;
+    private void Awake()
     {
         switch (GameManager.Instance.GetPlayer().GetComponent<PlayerStateMachine>().playerState)
         {
             case PlayerStates.BLUE:
                 // GetComponent<MeshRenderer>().material.SetColor("_Color", Color.blue);
-                GetComponent<MeshRenderer>().material.color = Color.blue;
+                GetComponent<DecalProjector>().material = blueDecalMat;
                 break;
             
             case PlayerStates.GREY:
-                GetComponent<MeshRenderer>().material.SetColor("_Color", Color.grey);
+                GetComponent<DecalProjector>().material = greyDecalMat;
                 break;
             
             case PlayerStates.BLACK:
-                GetComponent<MeshRenderer>().material.SetColor("_Color", Color.black);
+                GetComponent<DecalProjector>().material = blackDecalMat;
                 break;
 
             case PlayerStates.GREEN:
-                // GetComponent<MeshRenderer>().material.SetColor("_Color", Color.green);
-                GetComponent<MeshRenderer>().material.color = Color.green;
+                GetComponent<DecalProjector>().material = greenDecalMat;
                 break;
         }
     }
