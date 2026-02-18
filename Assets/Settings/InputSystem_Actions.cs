e51965d8b33277106267484f3ecd2b939e477b89
Aaron Deza Zapata <adezazapata@gmail.com>
2026-02-18 13:07:05 +0100

    Fix Initial Swimming action

--- Assets/Settings/InputSystem_Actions.cs
+++ Assets/Settings/InputSystem_Actions.cs
@@ -579,7 +579,7 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
                 {
                     ""name"": """",
                     ""id"": ""36e52cba-0905-478e-a818-f4bfcb9f3b9a"",
-                    ""path"": ""<Keyboard>/c"",
+                    ""path"": ""<Keyboard>/e"",
                     ""interactions"": """",
                     ""processors"": """",
                     ""groups"": ""Keyboard&Mouse"",
