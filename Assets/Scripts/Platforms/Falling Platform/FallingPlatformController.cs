fcb40da4c2191c15717ee0b8104cc9744c96e998
Aaron Deza Zapata <adezazapata@gmail.com>
2026-03-13 16:27:02 +0100

    Rotating plat done. Falling plat with errors

--- Assets/Scripts/Platforms/Falling Platform/FallingPlatformController.cs
+++ Assets/Scripts/Platforms/Falling Platform/FallingPlatformController.cs
@@ -1,6 +1,72 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 public class FallingPlatformController : MonoBehaviour
 {
+    public bool isActivated;
+    public float timeToFall;
+    public float timeToRegenerate;
+    public GameObject platform;
+
+    [SerializeField] private float currentTimeToFall;
+    [SerializeField] private float downfallSpeed;
+    [SerializeField] private Vector3 initialPosition;
+    [SerializeField] private Vector3 finalPosition;
     
+
+    private void OnEnable()
+    {
+        initialPosition = transform.position;
+        finalPosition = new Vector3(transform.position.x, transform.position.y - 20f, transform.position.z);
+        currentTimeToFall = timeToFall;
+    }
+
+
+    private void Update()
+    {
+        if (!isActivated) return;
+        
+        currentTimeToFall -= Time.deltaTime;
+        if (currentTimeToFall <= 0f)
+        {
+            FallingTrigger();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.gameObject.transform.parent = transform;
+            isActivated = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other) 
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.gameObject.transform.parent = null;
+        }
+        
+    }
+
+    private void FallingTrigger()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, finalPosition, Time.deltaTime * downfallSpeed);
+        
+        if (Vector3.Distance(transform.position, finalPosition) < 0.01f)
+        {
+            isActivated = false;
+            StartCoroutine(RegeneratePlatform());
+        }
+    }
+    
+    private IEnumerator RegeneratePlatform()
+    {
+        yield return new WaitForSeconds(timeToRegenerate);
+        transform.position = initialPosition;
+        currentTimeToFall = timeToFall;
+    }
 }
