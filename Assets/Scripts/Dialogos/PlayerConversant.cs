9f945072687d648d9db481e2f77b088424ecd93b
Carolmonty <Carolina.monteagudo@estudiant.enti.cat>
2026-04-27 22:33:47 +0200

    Prefabs created

--- Assets/Scripts/Dialogos/PlayerConversant.cs
+++ Assets/Scripts/Dialogos/PlayerConversant.cs
@@ -31,17 +31,9 @@ namespace Dialogo
         {
             DialogoNode[] children = currentDialogue.GetAllChildren(currentNode).ToArray();
 
-            // VERIFICACIÓN DE SEGURIDAD
             if (children.Length > 0)
             {
                 currentNode = children[0];
-                // Aquí podrías añadir lógica: trigger de eventos, cambio de speaker, etc.
-            }
-            else
-            {
-                // No hay más nodos, la conversación ha terminado.
-                // Opcional: Cerrar el diálogo aquí o dejar que el UI lo maneje
-                Debug.Log("Fin del diálogo");
             }
         }
 
